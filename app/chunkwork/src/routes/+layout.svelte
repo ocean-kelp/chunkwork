eb1e9d36e624b8f406cd9b5c3b26706547bad3ab
Xing Shuu <koren.yin@outlook.es>
2026-07-26 15:20:33 -0500

    feat: add Poppins font and update styles in app.css and layout

--- app/chunkwork/src/routes/+layout.svelte
+++ app/chunkwork/src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script>
   let { children } = $props();
   import "../app.css";
+
+  // fonts
+  import '@fontsource/poppins/400.css';
+  import '@fontsource/poppins/500.css';
+  import '@fontsource/poppins/600.css';
+  import '@fontsource/poppins/700.css';
+
 </script>
+
+
 
 {@render children()}
